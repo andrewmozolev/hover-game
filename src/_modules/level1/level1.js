4f1cc7f5b533b99dc36684565e675229c3a361d9
mozolev <avmozolev@yandex.ru>
2017-09-30 15:39:30 +0300

    init

---
+++ src/_modules/level1/level1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var Level1 = function() {
+  this.name = 'level1';
+  console.log('%s module', this.name);
+};
+
+module.exports = Level1;
---
+++ src/_modules/point/point.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var Chest = function() {
+  this.name = 'chest';
+  console.log('%s module', this.name);
+};
+
+module.exports = Chest;
